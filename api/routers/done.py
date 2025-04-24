2c63ccb60c121e460897ec892f58fe2fad4fe124
delideli0118zZZ <12345redboy118@gmail.com>
2025-04-24 10:15:13 +0900

    Create 작성

--- api/routers/done.py
+++ api/routers/done.py
@@ -9,6 +9,7 @@ router = APIRouter()
 # 예: /tasks/3/done -> 3번 할 일을 완료 처리한다.
 # ---------------------------------------------------------
 
+
 # 보충설명:
 # - PUT은 정보를 "설정하거나 수정"할 때 사용
 # - 주소 끝에 /done 은 "완료 상태 전환"을 의미미
@@ -23,10 +24,11 @@ async def mark_task_as_done(task_id: int):
 # 예: /tasks/3/done -> 3번 할 일을 완료 해제함
 # ---------------------------------------------------------
 
+
 # 보충 설명:
 # -DELETE는 정보를 "제거"할 때 사용
 # -여기선 "완료 표시"를 지운는 동작
 # - PUT과 DELETE는 반대 동작 (완료 <-> 해제)
 @router.delete("/tasks/{task_id}/done", response_model=None)
-async def unmark_task_as_done(task_idL int):
+async def unmark_task_as_done(task_idL: int):
     return
